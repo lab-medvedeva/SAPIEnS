ddee9b9343aa863b1a3ccd4d6a154ce24b31a581
Pavel Akhtyamov <akhtyamovpavel@gmail.com>
2022-01-30 19:37:13 +0300

    Added last version of notebooks

--- detect_cis_regions.R
+++ detect_cis_regions.R
@@ -6,19 +6,25 @@ OUTPUT = Sys.getenv('output')
 GENOME_PATH = Sys.getenv('genome_path')
 
 input_cds = make_atac_cds(paste(PREFIX,  CHROMOSOME, '.tsv', sep=''))
-
+print('Made cds')
 set.seed(2017)
 
 input_cds <- detectGenes(input_cds)
+
+print('Detected genes')
 input_cds <- estimateSizeFactors(input_cds)
+print('Estimated')
 input_cds <- reduceDimension(input_cds, max_components = 2, num_dim=6,
                       reduction_method = 'tSNE', norm_method = "none", verbose = T, check_duplicates = FALSE)
 
 
+print('Runned')
 tsne_coords <- t(reducedDimA(input_cds))
 row.names(tsne_coords) <- row.names(pData(input_cds))
-cicero_cds <- make_cicero_cds(input_cds, reduced_coordinates = tsne_coords)
 
+
+cicero_cds <- make_cicero_cds(input_cds, reduced_coordinates = tsne_coords)
+print('Made cicero cds')
 
 genome <- read.table(GENOME_PATH)
 sample_genome <- subset(genome, V1 == CHROMOSOME)
--- split_dataset.sh
+++ split_dataset.sh
@@ -1,9 +1,9 @@
 input_file=$1
 peaks_prefix=$2
-for i in $(seq 1 19)
+for i in $(seq 1 22)
 do
     cat $input_file | grep "chr${i}_" > ${peaks_prefix}_chr${i}.tsv
 done
 cat $input_file | grep "chrX_" > ${peaks_prefix}_chrX.tsv
 
-cat $input_file | grep "chrY_" > ${peaks_prefix}_chrY.tsv
+# cat $input_file | grep "chrY_" > ${peaks_prefix}_chrY.tsv
