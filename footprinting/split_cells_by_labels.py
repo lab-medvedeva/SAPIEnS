884b5bef392b4b73efc69d31753981e1bd760cc8
Pavel Akhtyamov <akhtyamovpavel@gmail.com>
2023-02-09 23:12:03 +0300

    Added reading for csv files

--- footprinting/split_cells_by_labels.py
+++ footprinting/split_cells_by_labels.py
@@ -1,4 +1,5 @@
 import os
+import scipy
 from scipy.io import mmread
 import pandas as pd
 import numpy as np
@@ -38,25 +39,51 @@ def get_peaks(folder, peaks_file='peaks.txt', counts_file='counts.mtx', barcodes
     return counts.tocsr(), peaks, barcodes
 
 
+def read_csv(path):
+    print(path)
+    if ('.txt' in path) or ('tsv' in path):
+        sep = '\t'
+    elif '.csv' in path:
+        sep = '\t'
+    else:
+        raise ValueError("File {} not in format txt or csv".format(path))
+    if 'txt' in path:
+        data = pd.read_csv(path, sep=sep, index_col=0).T.astype('float32')
+        genes = data.columns.values
+        barcode = data.index.values
+    elif 'tsv' in path or 'csv' in path:
+        data = pd.read_csv(path, sep=sep, index_col=0)
+        print(data.shape)
+        data = data.astype('float32')
+        genes = data.index.values
+        barcode = data.columns.values
+    return scipy.sparse.csr_matrix(data.values), genes, barcode
+
+
 def parse_args():
     parser = ArgumentParser('Split count matrix in 10X format to cell type peaks')
     parser.add_argument('--input', required=True, help='Path to input folder')
     parser.add_argument('--counts_file', required=True, help='Count matrix file', default='matrix.mtx')
-    parser.add_argument('--peaks_file', required=True, help='Path to peaks file', default='peaks.txt')
-    parser.add_argument('--barcodes_file', required=True, help='Path to barcodes file', default='barcodes.txt')
+    parser.add_argument('--peaks_file', help='Path to peaks file', default='peaks.txt')
+    parser.add_argument('--barcodes_file', help='Path to barcodes file', default='barcodes.txt')
     parser.add_argument('--labels', required=True, help='Path to labels')
     parser.add_argument('--iteration', required=True, help='Iteration to save')
+    parser.add_argument('--mode', required=True, choices=['10X', 'dense'])
     return parser.parse_args()
 
 
 def main(args):
-    counts, peaks, barcodes = get_peaks(
-        args.input,
-        counts_file=args.counts_file,
-        peaks_file=args.peaks_file,
-        barcodes_file=args.barcodes_file
-    )
+    if args.mode == '10X':
+        counts, peaks, barcodes = get_peaks(
+            args.input,
+            counts_file=args.counts_file,
+            peaks_file=args.peaks_file,
+            barcodes_file=args.barcodes_file
+        )
+    else:
+        counts, peaks, barcodes = read_csv(os.path.join(args.input, args.counts_file))
     
+    print(counts.shape)
     labels = pd.read_csv(args.labels, header=None, sep='\t')
     
     for cell_type in labels[1].unique():
